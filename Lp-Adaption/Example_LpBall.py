fix of q matix calculation. therefore radius r gets cal correctly

Lp-Adaption/Example_LpBall.py
@@ -50,21 +50,21 @@ class LpBallExample():
         """
         pn, r, mu, q = inopts
         if x.shape[1] == 1:
-            xtest = np.linalg.inv(q)/r @ (x - mu)
+            xtest = (np.linalg.inv(q)/r) @ (x - mu)
             if pn > 100:
-                if max(np.abs(xtest)) <= 1:
+                if np.max(np.abs(xtest)) <= 1:
                     return [1]
                 else:
                     return [0]
             else:
-                if sum(np.abs(xtest) ** pn) ** (1 / pn) <= 1:
+                if (np.sum(np.abs(xtest) ** pn) ** (1 / pn)) <= 1:
                     return [1]
                 else:
                     return [0]
         else:
             number = x.shape[1]
             b = (x - np.tile(mu, (1, number)))
-            xtest = np.linalg.inv(q) / b * r
+            xtest = (np.linalg.inv(q) /r) * b
 
             if pn > 100:
                 return [(np.max(np.abs(xtest), axis=1) <= 1).astype('int')]

Lp-Adaption/LpAdaption.py
@@ -396,7 +396,7 @@ class LpAdaption:
 
             # sampled vectors as input for oracle
             v = np.tile(mu, (1, p['popSize'].astype('int')))
-            arx = np.add(v, p['r'] * (Q @ arz))
+            arx = v + p['r'] * (Q @ arz)
 
             if self.isPlottingOn and self.isbSavingOn:
                 plot = PlotData()
@@ -642,7 +642,7 @@ class LpAdaption:
             # Adapt step size, (ball radius r)
             # Depends on the number of feasable Points,
             # Pseudo code line 15
-            p['r'] = np.round(p['r'] * pow(p['ss'],numfeas) * pow(p['sf'],p['popSize'] - numfeas),decimals=4)
+            p['r'] = p['r'] * pow(p['ss'],numfeas) * pow(p['sf'],p['popSize'] - numfeas)
             p['r'] = max(min(p['r'], p['rMax']), p['rMin'])
             # Adapt mu
             mu_old = mu
@@ -696,8 +696,8 @@ class LpAdaption:
                 if condC < p['condMax'] and condC > 0:
                     detdiagD = np.prod(diagD)  # normalize C
                     diagD = diagD / (detdiagD ** (1 / p['N']))
-                    Q = Bo * np.diag(diagD)
-                    invB = np.diag(1 / diagD) * np.transpose(Bo)
+                    Q = Bo @ np.diag(diagD)
+                    invB = np.diag(1 / diagD) @ np.transpose(Bo)
                 elif (counteval % self.opts.verboseModulo).any() == 0:
                     print('_______________________________________________\n'
                           'Condition of C is too large and regularized \n'

Lp-Adaption/LpBallSampling.py
@@ -1,7 +1,8 @@
 import numpy as np
 import sklearn
-from scipy.stats import gengamma
+from scipy.stats import gamma
 import random
+import matplotlib.pyplot as plt
 
 
 class LpBall:
@@ -22,21 +23,19 @@ class LpBall:
         :return: uniformly distributed real random vectors from the Lp-Ball
         '''
         # sample from gamma generalized distribution
-        psi = gengamma.rvs(a=(1/self.pnorm), c=1, size=(number,self.dim))
-        psi = [x ** (1 / self.pnorm) for x in psi]
-        psi = np.array(psi)
+        psi = np.random.default_rng().standard_gamma((1/self.pnorm),size=(number,self.dim))
+        psi = psi**(1/self.pnorm)
 
         # generate number x dim random signs for the samples psi to multiply with
         signs = np.random.randint(2, size=(number, self.dim))
         signs[signs == 0] = -1
-        X = np.multiply(psi,signs)
+        X = psi*signs
         # calc z = w^/dim, where w is random variable in [0,1]
         # copy z to dim dimensional array
         z = np.random.rand(number,1) ** (1 / self.dim)
-        i = np.abs(X)**self.pnorm
-        l = np.array(np.sum(np.abs(X) ** self.pnorm,axis=1)**(1. / self.pnorm))
-        m = np.transpose(np.tile(l,(self.dim,1)))
-        y = np.tile(z,(1,self.dim))*(X/m)
+        y = np.tile(z,(1,self.dim))*(X/np.tile(np.sum(np.abs(X)**self.pnorm,axis=1)**(1/self.pnorm),(1,self.dim)).reshape(self.dim,number).T)
         return y
+
+
 
 